6333d60b4c248ac44a07b5b7a6ac6c1ee208fc38
William Chavula <rumbani@gmail.com>
2025-05-16 09:42:38 -0500

    initialize tanstack query through provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ReactNode } from "react";
 import { ClerkProvider } from "@clerk/nextjs";
+import TanStackProviders from "@/providers/tanstack-provider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,7 +31,7 @@ export default function RootLayout({
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
-          {children}
+          <TanStackProviders>{children}</TanStackProviders>
         </body>
       </html>
     </ClerkProvider>
