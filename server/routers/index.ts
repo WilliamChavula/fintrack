5bcddf005c9e5ca00b1c592b1b762877423597e2
William Chavula <rumbani@gmail.com>
2025-05-21 09:10:48 -0500

    merge transactionRouter into trpc appRouter

--- server/routers/index.ts
+++ server/routers/index.ts
@@ -1,8 +1,13 @@
 import { mergeRouters } from "@/server/trpc";
 import { accountsRouter } from "./accounts";
 import { categoriesRouter } from "./categories";
+import { transactionsRouter } from "./transactions";
 
-export const appRouter = mergeRouters(accountsRouter, categoriesRouter);
+export const appRouter = mergeRouters(
+  accountsRouter,
+  categoriesRouter,
+  transactionsRouter,
+);
 
 // export type definition of API
 export type AppRouter = typeof appRouter;
