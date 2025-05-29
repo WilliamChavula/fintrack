0c167eb1f5e3d43bed5ed15c722b465e0e282c42
William Chavula <rumbani@gmail.com>
2025-05-29 14:50:18 -0500

    implement line chart

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,9 +1,11 @@
+import DataCharts from "@/components/charts/data-charts";
 import DataGrid from "@/components/data-grid";
 
 export default function Home() {
   return (
     <main className="mx-auto -mt-16 w-full max-w-screen-2xl pb-6">
       <DataGrid />
+      <DataCharts />
     </main>
   );
 }
